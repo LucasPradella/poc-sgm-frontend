06493e9bf6ee52913a64d9072522cc92e91fd83b
Lucas Pradella <lucas.pradella@hotmail.com>
2021-03-07 00:48:42 -0300

    add config heroku

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Alurapic
+# poc-sgm-frontend
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.7.
 
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/poc-sgm-frontend'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/poc-sgm-frontend/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/poc-sgm-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
